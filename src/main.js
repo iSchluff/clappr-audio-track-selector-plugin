eecab37c2c8d01d44430353d092bf205c48b2eab
Anton Schubert <ischluff@mailbox.org>
2020-04-05 18:07:17 +0200

    update build, rework package to work with both hls and dash

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
-import {Events, Styler, UICorePlugin, template} from 'Clappr'
+import {Events, Styler, UICorePlugin, template} from 'clappr'
 import pluginHtml from './public/audio-track-selector.html'
 import pluginStyle from './public/style.scss'
-
-const AUTO = -1
 
 export default class AudioTrackSelector extends UICorePlugin {
 
@@ -19,259 +17,180 @@ export default class AudioTrackSelector extends UICorePlugin {
   }
 
   get events() {
-    //console.log(' Audio-Track events');
     return {
-      'click [data-audio-track-selector-select]': 'onLevelSelect',
-      'click [data-audio-track-selector-button]': 'onShowLevelSelectMenu'
+      'click [data-audio-track-selector-lang]': 'handleLanguageSelect',
+      'click [data-audio-track-selector-button]': 'handleAudioTrackSelectorClick'
     }
   }
 
-  bindEvents() {
-    //console.log(' Audio-Track bindEvents');
-    this.listenTo(this.core, Events.CORE_READY, this.bindPlaybackEvents)
-    this.listenTo(this.core.mediaControl, Events.MEDIACONTROL_CONTAINERCHANGED, this.reload)
-    this.listenTo(this.core.mediaControl, Events.MEDIACONTROL_RENDERED, this.render)
-    this.listenTo(this.core.mediaControl, Events.MEDIACONTROL_HIDE, this.hideSelectLevelMenu)
+  get container() {
+    return this.core.activeContainer
+      ? this.core.activeContainer
+      : this.core.mediaControl.container
   }
 
-  unBindEvents() {
-    //console.log(' Audio-Track unBindEvents');
-    this.stopListening(this.core, Events.CORE_READY)
-    this.stopListening(this.core.mediaControl, Events.MEDIACONTROL_CONTAINERCHANGED)
-    this.stopListening(this.core.mediaControl, Events.MEDIACONTROL_RENDERED)
-    this.stopListening(this.core.mediaControl, Events.MEDIACONTROL_HIDE)
-    this.stopListening(this.core.getCurrentPlayback(), Events.PLAYBACK_LEVELS_AVAILABLE)
-    this.stopListening(this.core.getCurrentPlayback(), Events.PLAYBACK_LEVEL_SWITCH_START)
-    this.stopListening(this.core.getCurrentPlayback(), Events.PLAYBACK_LEVEL_SWITCH_END)
-    this.stopListening(this.core.getCurrentPlayback(), Events.PLAYBACK_BITRATE)
+  get playback() {
+    return this.core.activePlayback
+      ? this.core.activePlayback :
+      this.core.getCurrentPlayback();
+  }
+
+  bindEvents() {
+    if (Events.CORE_ACTIVE_CONTAINER_CHANGED)
+      this.listenTo(this.core, Events.CORE_ACTIVE_CONTAINER_CHANGED, this.reload)
+    else
+      this.listenTo(this.core.mediaControl, Events.MEDIACONTROL_CONTAINERCHANGED, this.reload)
+
+    this.listenTo(this.core, Events.CORE_READY, this.bindPlaybackEvents)
+    this.listenTo(this.core.mediaControl, Events.MEDIACONTROL_RENDERED, this.render)
+    this.listenTo(this.core.mediaControl, Events.MEDIACONTROL_HIDE, this._hideContextMenu)
   }
 
   bindPlaybackEvents() {
-    //console.log(' Audio-Track bindPlaybackEvents');
-      var currentPlayback = this.core.getCurrentPlayback()
-
-      this.listenTo(currentPlayback, Events.PLAYBACK_LEVELS_AVAILABLE, this.fillLevels)
-      //this.listenTo(currentPlayback, Events.PLAYBACK_LEVEL_SWITCH_START, this.startLevelSwitch)
-      //this.listenTo(currentPlayback, Events.PLAYBACK_LEVEL_SWITCH_END, this.stopLevelSwitch)
-      this.listenTo(currentPlayback, Events.PLAYBACK_BITRATE, this.updateCurrentLevelVideo)
-
-      //console.log(' Audio-Track bindPlaybackEvents'+currentPlayback);
-      var playbackLevelsAvaialbeWasTriggered = currentPlayback.levels && currentPlayback.levels.length > 0
-      playbackLevelsAvaialbeWasTriggered && this.fillLevels(currentPlayback.levels)
+      this.listenTo(this.playback, Events.PLAYBACK_LEVELS_AVAILABLE, this._handleLevels)
+      this.listenTo(this.playback, Events.PLAYBACK_BITRATE, this._handleAdaptation)
+      this.listenTo(this.playback, Events.PLAYBACK_PLAY, this._handlePlay)
   }
 
   reload() {
-    //console.log(' Audio-Track reload');
-    this.unBindEvents()
+    this.stopListening()
     this.bindEvents()
     this.bindPlaybackEvents()
   }
 
   shouldRender() {
-    //console.log(' Audio-Track shouldRender');
-    if (!this.core.getCurrentContainer()) return false
+    if (!this.container) return false;
+    if (!this.playback) return false;
 
-    var currentPlayback = this.core.getCurrentPlayback()
-    if (!currentPlayback) return false
+    // Only care if we have at least 2 languages to choose from
+    var hasChoice = !!(this.languages && this.languages.size > 1);
 
-    var respondsToCurrentLevel = currentPlayback.currentLevel !== undefined
-    // Only care if we have at least 2 to choose from
-    var hasLevels = !!(this.audiotrack && this.audiotrack.length > 1)
-
-    //console.log(' Audio-Track shouldRender ('+respondsToCurrentLevel+')('+hasLevels+')');
-    
-    return respondsToCurrentLevel && hasLevels
+    return hasChoice
   }
 
   render() {
-    //console.log(' Audio-Track render');
     if (this.shouldRender()) {
       var style = Styler.getStyleFor(pluginStyle, {baseUrl: this.core.options.baseUrl})
 
-      this.$el.html(this.template({'levels':this.audiotrack, 'title': this.getTitle()}))
+      this.$el.html(this.template({
+        'title': this._getTitle(),
+        'languages': this.languages
+      }));
       this.$el.append(style)
       this.core.mediaControl.$('.media-control-right-panel').append(this.el)
-      this.highlightCurrentLevel()
+      this._highlightCurrentElement()
     }
     return this
   }
 
-  fillLevels(levels, initialLevel = AUTO) {
-    //console.log(' Audio-Track fillLevels');
-    
-    if (this.core.getCurrentPlayback()._hls.audioTracks === undefined) return
-    if (this.core.getCurrentPlayback()._hls.audioTracks.length == 0) return
-    
-    //console.log('start filling in audio tracks');
-      
-    if (this.selectedLevelId === undefined) this.selectedLevelId = initialLevel
-      
-    //this.audiotrack = levels
-    this.audiotrack = this.core.getCurrentPlayback()._hls.audioTracks
-    //console.log(this.audiotrack);
-      for(var x=0;x<this.audiotrack.length;x++)
-      {
-        this.audiotrack[x].id=x;
-      }
-      
-      
-      for(var x=0;x<this.audiotrack.length;x++)
-      {
-        //console.log (x);
-        if(this.audiotrack[x].groupId == this.core.getCurrentPlayback()._hls.streamController.levels[+this.core.getCurrentPlayback()._hls.streamController.level].attrs.AUDIO)
-        {
-          
-          //console.log('a group match, selecting default('+this.audiotrack[x].groupId +')('+this.core.getCurrentPlayback()._hls.streamController.levels[+this.core.getCurrentPlayback()._hls.streamController.level].attrs.AUDIO+')');
-          if(this.audiotrack[x].default == true)
-          {
-            //console.log('selecting');
-            //console.log(this.audiotrack[x]);
-            this.selectedLevelId = x
-            
-            this.currentLevel=this.audiotrack[this.selectedLevelId]
-            this.core.getCurrentPlayback()._hls.audioTrack= this.selectedLevelId;          
-            this.highlightCurrentLevel();          
-          }
-        }
-      }
-      
-    this.configureLevelsLabels()
-    //
-    this.render()
-    
-    var group = this.core.getCurrentPlayback()._hls.streamController.levels[this.core.getCurrentPlayback()._hls.streamController.level].attrs.AUDIO;
-    this.agroupElement().addClass('hidden');
-    this.$('.audio_track_selector ul a[data-level-group-selector-select="'+group+'"]').parent().removeClass('hidden')
-    
-  }
+  _setLanguage(language) {
+    console.log("setLanguage", language);
 
-  configureLevelsLabels() {
-    //console.log(' Audio-Track configureLevelsLabels');
-    if (this.core.options.AudioTrackSelectorConfig === undefined) return
+    // custom voc dash-shaka-playback
+    if (this.playback.selectAudioLanguage) {
+      this.nextLanguage = language;
+      this._startTrackSwitch();
+      this.playback.selectAudioLanguage(language);
 
-    for (var levelId in (this.core.options.AudioTrackSelectorConfig.labels || {})) {
-      //console.log(' Audio-Track configureLevelsLabels levelId:'+levelId);
-      levelId = parseInt(levelId, 10)
-      var thereIsLevel = !!this.findLevelBy(levelId)
-      thereIsLevel && this.changeLevelLabelBy(levelId, this.core.options.AudioTrackSelectorConfig.labels[levelId])
+    // hlsjs playback
+    } else if(this.playback._hls) {
+
+      // hlsjs may have multiple audiotracks with the same language
+      // this will just switch to the first one
+      const track = this.playback._hls.audioTracks.find((track) => track.lang == language)
+      if (!track) return;
+
+      this.playback._hls.audioTrack = track.id;
+      this.activeLanguage = language;
+      this._highlightCurrentElement();
     }
   }
 
-  findLevelBy(id) {
-    //console.log(' Audio-Track findLevelBy');
-    //console.log(id);
-    //console.log(this.audiotrack);
-    var foundLevel
-    this.audiotrack.forEach((level) => { if (level.id === id) {foundLevel = level} })
-    return foundLevel
-  }
+  _fillLanguages() {
+    // custom voc dash-shaka-playback
+    if (this.playback.audioLanguages) {
+      this.languages = new Set(this.playback.audioLanguages);
 
-  changeLevelLabelBy(id, newLabel) {
-    //console.log(' Audio-Track changeLevelLabelBy');
-    this.audiotrack.forEach((level, index) => {
-      if (level.id === id) {
-        this.audiotrack[index].name = newLabel
-      }
-    })
-  }
+    // hlsjs playback
+    } else if (this.playback._hls) {
+      const audioTracks = this.playback._hls.audioTracks;
+      const trackId = this.playback._hls.audioTrack;
+      const current = audioTracks.find((track) => track.id == trackId);
 
-  onLevelSelect(event) {
-    //console.log(' Audio-Track onLevelSelect ('+event.target.dataset.audioTrackSelectorSelect+')');
-    //console.log('||'+event.target.dataset.audioTrackSelectorSelect.substr(6)+'||');
-    
-//     this.selectedLevelId = parseInt(event.target.dataset.audioTrackSelectorSelect.substr(6), 10)
-    this.selectedLevelId = event.target.dataset.audioTrackSelectorSelect.substr(6)
-    //console.log(''+this.currentLevel.id+' == '+this.selectedLevelId+'');
-    if (this.currentLevel.id == this.selectedLevelId) return false;
-    this.currentLevel=this.audiotrack[this.selectedLevelId]
-    //console.log(this.audiotrack[this.selectedLevelId]);        
-    //console.log(this.core.getCurrentPlayback()._hls);
-    //console.log(this.core.getCurrentPlayback()._hls.audioTracks);
-    
-    //this.core.getCurrentPlayback()._hls.audioTrack= this.audiotrack[this.selectedLevelId];
-    this.core.getCurrentPlayback()._hls.audioTrack= this.selectedLevelId;
-
-    this.toggleContextMenu()
-    this.highlightCurrentLevel();
-    event.stopPropagation()
-    
-    return false
-  }
-  onShowLevelSelectMenu(event) { this.toggleContextMenu() }
-
-  hideSelectLevelMenu() { this.$('.audio_track_selector ul').hide() }
-
-  toggleContextMenu() { this.$('.audio_track_selector ul').toggle() }
-
-  buttonElement() { return this.$('.audio_track_selector button') }
-
-  levelElement(id) {  return this.$('.audio_track_selector ul a'+(!isNaN(id) ? '[data-audio-track-selector-select="audio_'+id+'"]' : '')).parent() }
-  agroupElement(gid) { return this.$('.audio_track_selector ul a'+(!isNaN(gid) ? '[data-level-group-selector-select="'+gid+'"]' : '')).parent() }
-
-  getTitle() { return (this.core.options.AudioTrackSelectorConfig || {}).name }
-
-  startLevelSwitch() { this.buttonElement().addClass('changing') }
-
-  stopLevelSwitch() { this.buttonElement().removeClass('changing') }
-
-  updateText(level) {
-    //console.log(' Audio-Track updateText ('+level+')' );
-    if (level === -1) {
-      this.buttonElement().text(this.findLevelBy(this.audiotrack[0].id).name)
-      this.selectedLevelId = this.audiotrack[0].id;
-    }
-    else {
-      this.buttonElement().text(this.findLevelBy(this.audiotrack[level].id).name)
-    }
-  }
-  updateCurrentLevel(info) {
-//    console.log(' Audio-Track updateCurrentLevel');
-//     console.log(info);
-    var level = this.findLevelBy(info.id)
-//     console.log(level);
-    this.currentLevel = level ? level : null
-    this.highlightCurrentLevel()
-  }
-  updateCurrentLevelVideo(info) {
-    //console.log(' Audio-Track updateCurrentLevelVideo');
-    
-    if(this.audiotrack ==undefined) return;
-    if(this.audiotrack.length == 0 ) return;
-    
-    //console.log(' Audio-Track updateCurrentLevelVideo2');
-    
-    var group = this.core.getCurrentPlayback()._hls.streamController.levels[info.level].attrs.AUDIO;
-    this.agroupElement().addClass('hidden');
-    this.$('.audio_track_selector ul a[data-level-group-selector-select="'+group+'"]').parent().removeClass('hidden')
-
-    for(var x=0;x<this.audiotrack.length;x++)
-    {
-      if(this.audiotrack[x].groupId == this.core.getCurrentPlayback()._hls.streamController.levels[info.level].attrs.AUDIO)
-      {
-        //this.agroupElement(this.audiotrack[x].groupId).removeClass('hidden')        
-//         console.log('a group match, selecting default('+this.audiotrack[x].groupId +')('+this.core.getCurrentPlayback()._hls.streamController.levels[info.level].attrs.AUDIO+')');
-        if(this.audiotrack[x].default == true)
-        {
-          //console.log('selecting');
-          //console.log(this.audiotrack[x]);
-          this.selectedLevelId = x
-          
-          if (this.currentLevel.id == this.selectedLevelId) return false;
-          
-          this.currentLevel=this.audiotrack[this.selectedLevelId]
-          this.core.getCurrentPlayback()._hls.audioTrack= this.selectedLevelId;          
-          this.highlightCurrentLevel();          
-        }
+      this.languages = new Set(audioTracks.map((track) => track.lang));
+      if (current) {
+        this.activeLanguage = current.lang;
       }
     }
-    //
-    //
+
+    this.render();
   }
-  highlightCurrentLevel() {
-    //console.log(' Audio-Track highlightCurrentLevel ('+this.currentLevel.id+')');
-    //console.log(this.currentLevel);
-    this.levelElement().removeClass('current')
-    this.levelElement(this.currentLevel.id).addClass('current')
-    this.updateText(this.selectedLevelId)
+
+  handleLanguageSelect(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const selected = event.target.dataset.audioTrackSelectorLang;
+
+    if (this.activeLanguage == selected) return false;
+    this._setLanguage(selected);
+    this._toggleContextMenu();
+    return false;
+  }
+
+  // Handles adaptation event, currently only from shaka-playback
+  _handleAdaptation(variant) {
+    if (variant.language) {
+      this.activeLanguage = variant.language;
+      this._highlightCurrentElement();
+      if (variant.language == this.nextLanguage) {
+        this._stopTrackSwitch();
+      }
+    }
+  }
+
+  // shaka-playback knows languages on level event
+  _handleLevels() {
+    this._fillLanguages();
+  }
+
+  // hlsjs-playback only knows languages on play
+  _handlePlay() {
+    if (this.playback._hls) {
+      this._fillLanguages();
+    }
+  }
+
+  handleAudioTrackSelectorClick(event) {
+    this._toggleContextMenu()
+  }
+
+  _toggleContextMenu() {
+    this.$('.audio_track_selector ul').toggle()
+  }
+
+  _hideContextMenu() { this.$('.audio_track_selector ul').hide() }
+
+  _getLanguageElement(language = null) {
+    if (language)
+      return this.$('.audio_track_selector a[data-audio-track-selector-lang="' + language + '"]')
+    else
+      return this.$('.audio_track_selector a');
+  }
+
+  _getButtonElement() {
+    return this.$('.audio_track_selector button')
+  }
+
+  _getTitle() { return (this.core.options.levelSelectorConfig || {}).title }
+  _startTrackSwitch() { this._getButtonElement().addClass('changing') }
+  _stopTrackSwitch() { this._getButtonElement().removeClass('changing') }
+
+  _highlightCurrentElement() {
+    if (!this.activeLanguage)
+      return;
+    this._getLanguageElement().removeClass('current')
+    this._getLanguageElement(this.activeLanguage).addClass('current')
+    this._getButtonElement().text(this.activeLanguage);
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,59 @@
-var path = require('path');
-var webpack = require('webpack');
-var filename = 'audio-track-selector.js'
+const path = require('path');
+const filename = 'audio-track-selector.js'
 
 module.exports = {
-  entry: path.resolve(__dirname, 'index.js'),
+  entry: path.resolve(__dirname, 'src/main.js'),
   externals: {
-    "Clappr": "Clappr",
-    "clappr-zepto": "clappr-zepto"
+    clappr: {
+      amd: 'clappr',
+      commonjs: 'clappr',
+      commonjs2: 'clappr',
+      root: 'Clappr'
+    }
   },
   module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel',
-        query: {
-            compact: true,
-        }
-      },
-      {
-        test: /\.scss$/,
-        loaders: ['css', 'sass?includePaths[]='
-          + path.resolve(__dirname, './node_modules/compass-mixins/lib')
-          + '&includePaths[]='
-          + path.resolve(__dirname, './node_modules/clappr/src/base/scss')
-          + '&includePaths[]='
-          + path.resolve(__dirname, './src/base/scss')
-        ],
-        include: path.resolve(__dirname, 'src'),
-      },
-      {
-        test: /\.html/, loader: 'html?minimize=false'
-      },
-    ],
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      query: {
+          compact: true,
+      }
+    }, {
+      test: /\.s[ac]ss$/i,
+      use: [
+        // Creates `style` nodes from JS strings
+        // 'style-loader',
+        // Translates CSS into CommonJS
+        'css-loader',
+        // Compiles Sass to CSS
+        'sass-loader',
+      ],
+      // test: /\.scss$/,
+      // loaders: ['css-loader', 'sass-loader?includePaths[]='
+      //   + path.resolve(__dirname, './node_modules/compass-mixins/lib')
+      //   + '&includePaths[]='
+      //   + path.resolve(__dirname, './node_modules/clappr/src/base/scss')
+      //   + '&includePaths[]='
+      //   + path.resolve(__dirname, './src/base/scss')
+      // ],
+      include: path.resolve(__dirname, 'src'),
+    }, {
+      test: /\.html$/,
+      loader: 'html-loader',
+      options: {
+        minimize: false
+      }
+    }],
   },
   resolve: {
-    extensions: ['', '.js'],
+    extensions: ['.js'],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '<%=baseUrl%>/',
+    // publicPath: '<%=baseUrl%>/',
     filename: filename,
     library: 'AudioTrackSelector',
+    libraryExport: 'default',
     libraryTarget: 'umd',
   },
 };
